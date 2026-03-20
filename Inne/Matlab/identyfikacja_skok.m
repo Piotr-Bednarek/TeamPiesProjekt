b524a139d99031af274844dc22c2d835c98bc774
Mateusz Banaszak <mateusz.banaszak2412@gmail.com>
2026-03-20 11:30:30 +0100

    Aktualizacja skryptów identyfikacji i parametrów regulatora, dodanie wykresów PDF

--- Inne/Matlab/identyfikacja_skok.m
+++ Inne/Matlab/identyfikacja_skok.m
@@ -3,53 +3,55 @@ close all;
 clc;
 
 s= tf('s');
-%Parametry regulatora
-Ti=1;
-Kp=1;
-Td=1;
+
+%Ti=1;
+%Kp=1;
+%Td=1;
 
 
-% --- KONFIGURACJA ---
-A_stopnie = 25;          % Jaki skok zadałeś w eksperymencie (w stopniach)?
-%A_rad = deg2rad(A_stopnie); % Przeliczamy na radiany (fizyka lubi radiany!)
 
-% --- WCZYTANIE DANYCH ---
-data = readmatrix('wychylenie-125.csv'); 
+A_stopnie = 9;        
+
+data = readmatrix('wychylenie-109.csv'); 
 t_raw = data(:, 1);     % Czas
-y_raw = data(:, 3);     % Pozycja kulki [m] (upewnij się że metry, nie mm!)
+y_raw = data(:, 3);     % Pozycja piłki [mm]
+figure(1)
+plot(t_raw, y_raw); title('Zależność pozycji piłki od czasu (A = 40^\circ)');
+xlabel('Czas (s)')
+ylabel('Pozycja piłki (mm)')
 
-plot(t_raw, y_raw); title('Znajdź zakres czasu startu i końca');
 
-
-idx = t_raw > 1.1 & t_raw < 2.2; 
+idx = t_raw > 1.35 & t_raw < 3.095; 
 
 t = t_raw(idx);
 y = y_raw(idx);
 
-% Przesuwamy czas i pozycję do zera (start paraboli w (0,0))
+% Przesunięcie czasu i pozycji do 0, żeby parabola wychodziła z punktu
+% (0,0)
+
 t = t - t(1);
 y = y - y(1);
 
-% --- OBLICZANIE K (POLYFIT) ---
-% Dopasowujemy wielomian 2. stopnia: y = p(1)*t^2 + p(2)*t + p(3)
+
+%Dopsaowanie do wielomianu 2 stopnia
 p = polyfit(t, y, 2);
 
 % Współczynnik przy t^2 to nasze p(1).
 % Z fizyki wiemy, że y = 0.5 * (K * A) * t^2
 % Zatem: p(1) = 0.5 * K * A
-
-%K_obl = (2 * p(1)) / A_rad;  % Jeśli model ma brać radiany
 K_obl = (2 * p(1)) / A_stopnie; % Jeśli model ma brać stopnie
 
 fprintf('Współczynnik paraboli a (przy t^2): %.4f\n', p(1));
 fprintf('Wyliczone wzmocnienie K: %.4f\n', K_obl);
 
-% --- WERYFIKACJA ---
 y_model = p(1)*t.^2 + p(2)*t + p(3);
-figure;
+
+figure(2)
 plot(t, y, 'b.', t, y_model, 'r-', 'LineWidth', 2);
+xlabel('Czas (s)')
+ylabel('Pozycja piłki (mm)')
 legend('Dane pomiarowe', 'Dopasowana parabola');
-title(['Dopasowanie modelu K/s^2. K = ' num2str(K_obl)]);
+title(['Dopasowanie modelu K/s^2 dla A = 40^\circ.    K = ' num2str(K_obl)]);
 grid on;
 
 
--- Inne/Matlab/wczytanie_identyfikacji.m
+++ Inne/Matlab/wczytanie_identyfikacji.m
@@ -1,10 +1,7 @@
 data = readmatrix('prbs.csv');
-%data1 = readmatrix('identyfikacja-6.csv');
 
-u = data(:,6);  % <-- Tu wpisz numer kolumny z PWM
-y = data(:, 3);  % <-- Tu wpisz numer kolumny z ToF
 
-%u1 = data1(:, 4);  % <-- Tu wpisz numer kolumny z PWM
-%y1 = data1(:, 3);  % <-- Tu wpisz numer kolumny z ToF
+u = data(:,6);  
+y = data(:, 3);  
 
 Ts = 0.034;
